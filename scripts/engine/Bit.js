449eaaede4c239dfb764e14ef2d3bd2f71652b1e
micromajor <dorian@dorian.com>
2016-04-10 22:38:22 +0200

    ajout d un moteur de gestion

---
+++ scripts/engine/Bit.js
@@ -0,0 +1,7 @@
+var Bit = Bit || {};
+
+function Bit(x,y, level){
+	this.x = x;
+	this.y = y;
+	this.level = level;
+}
---
+++ scripts/engine/Manager.js
@@ -0,0 +1,7 @@
+var Manager = Manager || {};
+
+
+function Manager(){
+	
+	this.bitMap  = {}
+}
